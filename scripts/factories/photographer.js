dbbb42a8181e3aa6aec4299f46766497bcc24888
Pascal Allau <pascal.allau@zohomail.eu>
2022-06-12 19:27:23 +0200

    feat: photographers data recovery and displat

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,21 @@
+function addElement(parent, tag, text, {...attributes}) {
+    const el = document.createElement(tag);
+    el.innerHTML = text;
+    Object.keys(attributes).forEach(key => el.setAttribute(key, attributes[key]));
+    parent.appendChild(el);
+}
 export default function photographerFactory(data) {
-    const { name, portrait } = data;
-
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/${data.portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement('article');
-        const img = document.createElement('img');
-        img.setAttribute('src', picture);
-        const h2 = document.createElement('h2');
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        article.classList.add('photographer');
+        addElement(article, 'img', '', { class: 'photographer_picture', src: picture });
+        addElement(article, 'h2', `${ data.name }`, { class: 'photographer_name' });
+        addElement(article, 'p', `${ data.city }, ${ data.country }`, { class: 'photographer_location' });
+        addElement(article, 'p', `${ data.tagline }`, { class: 'photographer_tagline' });
+        addElement(article, 'p', `${ data.price }€/jour`, { class: 'photographer_price' });
         return article;
     }
-    return { name, picture, getUserCardDOM };
+    return { getUserCardDOM };
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -2,30 +2,9 @@ import '../../css/style.scss';
 import photographerFactory   from "../factories/photographer";
 
 async function getPhotographers() {
-    // Penser à remplacer par les données récupérées dans le json
-    const photographers = [
-        {
-            name: 'Ma data test',
-            id: 1,
-            city: 'Paris',
-            country: 'France',
-            tagline: 'Ceci est ma data test',
-            price: 400,
-            portrait: 'account.png',
-        },
-        {
-            name: 'Autre data test',
-            id: 2,
-            city: 'Londres',
-            country: 'UK',
-            tagline: 'Ceci est ma data test 2',
-            price: 500,
-            portrait: 'account.png',
-        },
-    ];
-    // et bien retourner le tableau photographers seulement une fois
+    const photographers = await fetch('data/photographers.json').then(response => response.json()).then(data => data.photographers);
     return {
-        photographers: [...photographers, ...photographers, ...photographers],
+        photographers
     };
 }
 
