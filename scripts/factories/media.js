93e0c7e61a3862e8e7ff1529a5d6197a7dcb9352
Pascal Allau <pascal.allau@zohomail.eu>
2022-07-09 22:46:17 +0200

    update photographer_media class name, replace ternary by if else and del comments

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -2,10 +2,9 @@ import addElement from '../utils/addElement';
 
 export default function mediaFactory(data) {
     const { id, photographerId, title, image, video, likes } = data;
-    // const heart = `assets/icons/heart.svg`;
     const userImage = `assets/images/${photographerId}/${image}`;
     const userVideo = `assets/images/${photographerId}/${video}`;
-    const mediaUser = document.querySelector('.photographer_media-user');
+    const mediaUser = document.querySelector('.photographer_media');
 
     function getUserMediaDOM() {
         const article = addElement(mediaUser, 'article', '', {
@@ -39,7 +38,11 @@ export default function mediaFactory(data) {
             });
         }
 
-        video ? setUserVideo() : setUserImage();
+        if (video) {
+            setUserVideo();
+        } else {
+            setUserImage();
+        }
         const footer = addElement(article, 'footer', '', {
             class: 'product-footer',
         });
@@ -50,14 +53,6 @@ export default function mediaFactory(data) {
             <path d="M9.5 18.35L8.23125 17.03C3.725 12.36 0.75 9.28 0.75 5.5C0.75 2.42 2.8675 0 5.5625 0C7.085 0 8.54625 0.81 9.5 2.09C10.4537 0.81 11.915 0 13.4375 0C16.1325 0 18.25 2.42 18.25 5.5C18.25 9.28 15.275 12.36 10.7688 17.04L9.5 18.35Z" fill="currentcolor"/>
         </svg>
         `;
-        // addElement(footer, 'p', title, { class: 'product-title' });
-        // addElement(footer, 'span', likes, { class: 'product-likes' });
-        // addElement(footer, 'img', '', {
-        //     class: 'product-heart',
-        //     alt: 'likes',
-        //     role: 'img',
-        //     src: heart,
-        // });
         return article;
     }
 
